e07c6219befca71bc51c4bc45ba6d40b665c20b2
Eric Pan <ericpan122@gmail.com>
2024-04-09 13:02:00 -0400

    some final changes

---
+++ src/Character.java
@@ -0,0 +1,22 @@
+public class Character {
+    private int row;
+    private int col;
+//character
+    public Character(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void move(int newRow, int newCol) {
+        this.row = newRow;
+        this.col = newCol;
+    }
+}
